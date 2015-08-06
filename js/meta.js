b3b4a24f700bfe0cf8cc989f80d30693afaf948f
Владислав <vladis1ove81@gmail.com>
2015-08-06 15:01:50 +0600

    no message

---
+++ js/meta.js
@@ -0,0 +1,5 @@
+// if($( window ).width() < 768) {
+// 	$( "title" ).after('<meta name="viewport" content="width=750,user-scalable=no">');
+// } else {
+// 	$( "title" ).after('<meta name="viewport" content="width=device-width,initial-scale=1.0">');
+// }
